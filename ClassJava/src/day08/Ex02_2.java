39d0596d01f1b86c6a2f8decac1bd368ac349fca
user <user@user-PC>
2019-10-21 19:25:13 +0900

    day08 last edit

--- ClassJava/src/day08/Ex02_2.java
+++ ClassJava/src/day08/Ex02_2.java
@@ -3,10 +3,11 @@ package day08;
 public class Ex02_2 extends Rect {
 
 	public Ex02_2() {
+		super(10, 8);
+		System.out.println("사각형의 넓이는 : "+area);
 	}
 
 	public static void main(String[] args) {
-
+		new Ex02_2();
 	}
-
 }
--- ClassJava/src/test/Check.java
+++ ClassJava/src/test/Check.java
@@ -5,7 +5,7 @@ public class Check {
 	public Check() {
 		Test b = new Test();
 		System.out.println(b.b);
-		System.out.println(b.a);
+		System.out.println(Test.a);
 	}
 	public static void main(String[] args) {
 		new Check();
